7d66fa2e5cefdabb8eb88e91b6a2cd8a9d80001f
Randall LaPoint, Jr <rplapointjr@gmail.com>
2025-06-03 10:38:59 -0500

    (fix): EsLint build error

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { useAuth } from "@clerk/nextjs";
