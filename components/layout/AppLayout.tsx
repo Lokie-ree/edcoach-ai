3dc57852b27cb8f86f011e7a611fe8d0ec4b9a5d
Randall LaPoint, Jr <rplapointjr@gmail.com>
2025-07-10 15:54:49 -0500

    (fix): build errors and package.json updated

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/layout/header";
+import Header from "@/components/layout/Header";
 import MaxWidthWrapper from "@/components/layout/MaxWidthWrapper";
 
 export default function AppLayout({
